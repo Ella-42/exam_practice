b8c3885756c195ec02414e886ce384828438f19c
Ella-42 <Ellakazam.h@gmail.com>
2023-12-01 00:39:05 +0100

    made almost norm complient, just to see, suprisingly, only went over function line limit(auto_push)

--- 4th_rank_exam/test.c
+++ 4th_rank_exam/test.c
@@ -6,7 +6,7 @@
 /*   By: lpeeters <lpeeters@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 19:42:54 by lpeeters          #+#    #+#             */
-/*   Updated: 2023/11/30 00:26:57 by lpeeters         ###   ########.fr       */
+/*   Updated: 2023/12/01 00:21:48 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,53 @@
 
 int	main(int ac, char **av, char **env)
 {
+	pid_t	cmd1;
+	pid_t	cmd2;
+	pid_t	cmd3;
+	int		pfd[2];
+	int		status;
+
 	(void)ac;
 	(void)av;
-	int pfd[2];
-	int status;
 	pipe(pfd);
-	pid_t cmd1 = fork();
+	cmd1 = fork();
 	if (!cmd1)
 	{
 		close(pfd[0]);
 		dup2(pfd[1], 1);
-		execve("/bin/ls", (char *[]){"/bin/ls", "-l", NULL}, env);
+		close(pfd[1]);
+		execve("/bin/ls", (char *[]){"ls", NULL}, env);
 	}
 	else
 	{
-		close(pfd[1]);
 		waitpid(cmd1, &status, 0);
+		close(pfd[1]);
 		dup2(pfd[0], 0);
+		close(pfd[0]);
 		pipe(pfd);
-		pid_t cmd2 = fork();
+		cmd2 = fork();
 		if (!cmd2)
 		{
 			close(pfd[0]);
 			dup2(pfd[1], 1);
+			close(pfd[1]);
 			execve("/bin/cat", (char *[]){"/bin/cat", "-e", NULL}, env);
 		}
 		else
 		{
+			waitpid(cmd1, &status, 0);
 			close(pfd[1]);
-			waitpid(cmd2, &status, 0);
 			dup2(pfd[0], 0);
-			pid_t cmd3 = fork();
+			close(pfd[0]);
+			cmd3 = fork();
 			if (!cmd3)
 			{
-				close(pfd[0]);
-				execve("/bin/wc", (char *[]){"wc", NULL}, env);
+				execve("/bin/ls", (char *[]){"ls", NULL}, env);
 			}
 			else
 			{
 				waitpid(cmd3, &status, 0);
+				close(pfd[1]);
 				printf("exit: %i\n", status);
 			}
 		}
