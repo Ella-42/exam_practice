84098b1dce3ae89b3ce7ccb4de0e98072e8760f6
Ella <EllaP.jobs@gmail.com>
2025-09-11 03:46:13 +0200

    changed max connections to listen for to OS defines value, now able to accept connections with exceptional performance(auto push :heart:)

--- 6th_rank_exam/mini_serv.c
+++ 6th_rank_exam/mini_serv.c
@@ -6,7 +6,7 @@
 /*   By: lpeeters <lpeeters@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 21:47:23 by lpeeters          #+#    #+#             */
-/*   Updated: 2025/09/08 23:52:44 by lpeeters         ###   ########.fr       */
+/*   Updated: 2025/09/11 03:44:16 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/ip.h>
+
+typedef struct s_client
+{
+	size_t id;
+	char buffer[1024];
+}	t_client;
 
 void error(const char *string, int fd)
 {
@@ -37,8 +43,41 @@ int main(int arguments, char *argument_list[])
 	server_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
 	int server_fd;
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 || bind(server_fd, (const struct sockaddr *)&server_address, sizeof(server_address)) || listen(server_fd, 10))
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 || bind(server_fd, (const struct sockaddr *)&server_address, sizeof(server_address)) || listen(server_fd, SOMAXCONN))
 		error(0, server_fd);
 
-	close(server_fd);
+	fd_set fds, read_fds;
+	FD_ZERO(&fds);
+	FD_SET(server_fd, &fds);
+
+	t_client clients[FD_SETSIZE];
+	int ready, last_fd = server_fd;
+	size_t last_id = 0;
+	for (;;)
+	{
+		read_fds = fds;
+		ready = select(last_fd + 1, &read_fds, NULL, NULL, NULL);
+
+		for (int fd = 3; ready > 0; fd++)
+		{
+			if (!FD_ISSET(fd, &read_fds)) continue;
+
+			ready--;
+
+			if (fd == server_fd)
+			{
+				int client_fd = accept(server_fd, NULL, NULL);
+				if (client_fd == -1) continue;
+
+				if (client_fd > last_fd) last_fd = client_fd;
+				FD_SET(client_fd, &fds);
+				clients[client_fd].id = last_id++;
+				memset(&(clients[client_fd].buffer), 0, 1024);
+
+				write(STDOUT_FILENO, "New client accepted\n", 20);
+
+				continue;
+			}
+		}
+	}
 }
